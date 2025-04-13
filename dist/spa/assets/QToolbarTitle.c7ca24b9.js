98c2db85beca8e34bed65fc6af80f70f6b2f7d7c
samuelitwaru <samuelitwaru@gmail.com>
2025-04-13 11:06:27 +0300

    more

---
+++ dist/spa/assets/QToolbarTitle.c7ca24b9.js
@@ -0,0 +1,1 @@
+import{m as a,n as t,p as l,I as r}from"./index.0f8885c7.js";var n=a({name:"QToolbarTitle",props:{shrink:Boolean},setup(o,{slots:s}){const e=t(()=>"q-toolbar__title ellipsis"+(o.shrink===!0?" col-shrink":""));return()=>l("div",{class:e.value},r(s.default))}});export{n as Q};
---
+++ dist/spa/assets/use-dark.bcc088a8.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index.0f8885c7.js";const u={dark:{type:Boolean,default:null}};function n(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{u as a,n as u};
