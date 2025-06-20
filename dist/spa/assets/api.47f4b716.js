9e09043decad5f02369041849f4622cd0e528684
samuelitwaru <samuelitwaru@gmail.com>
2025-06-20 21:57:01 +0300

    more

--- dist/spa/assets/api.47f4b716.js
+++
@@ -1,1 +0,0 @@
-import{api as t}from"./axios.37f31599.js";async function s(){return(await t.get("calls/")).data}async function r(){return(await t.get("themes/")).data}export{s as a,r as g};
---
+++ dist/spa/assets/use-dark.dbcb9d7c.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index.d46529e0.js";const u={dark:{type:Boolean,default:null}};function n(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{u as a,n as u};
